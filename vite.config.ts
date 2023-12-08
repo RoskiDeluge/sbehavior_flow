23a48961cb38b1b548735a50d469ef8366802732
Roberto Delgado <RoskiDeluge@users.noreply.github.com>
2023-12-08 22:45:07 +0000

    ran build and viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/sbehavior_flow/', // Set the base to match your GitHub Pages URL path
   plugins: [react()],
   server: {
     port: 3000
